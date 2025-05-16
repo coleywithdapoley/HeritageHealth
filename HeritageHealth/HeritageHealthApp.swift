9895826bb4798cc87c24b38a303cc0bb8eb7aa61
coleywithdapoley <cobb.cole@gmail.com>
2025-05-16 08:34:31 -0400

    Initial Commit

---
+++ HeritageHealth/HeritageHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeritageHealthApp.swift
+//  HeritageHealth
+//
+//  Created by Cole Cobb on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct HeritageHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeritageHealthTests/HeritageHealthTests.swift
@@ -0,0 +1,17 @@
+//
+//  HeritageHealthTests.swift
+//  HeritageHealthTests
+//
+//  Created by Cole Cobb on 5/16/25.
+//
+
+import Testing
+@testable import HeritageHealth
+
+struct HeritageHealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
